5d565ce81d4d77e493b85c07c8b5c6e54330895e
ivan.liu <403704409@qq.com>
2017-02-13 21:04:42 +0800

    first commit

---
+++ model/article.js
@@ -0,0 +1,26 @@
+// const connection = require('./init.js')
+const moment = require('moment')
+var mysql = require('mysql');
+var connection = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'root',
+  password : '',
+  database : 'blog'
+});
+
+
+exports.getArticleList = () => {
+	return new Promise((resolve, reject) => {
+		connection.query('select * from article', (err, res, fields) => {
+			if (err){
+				reject(err)
+				return 
+			}
+			res.forEach(val => {
+				val.utime = moment(val.utime).format('YYYY-MM-DD HH:mm:SS')
+			})
+			resolve(res)
+		})
+	})
+	
+}
---
+++ routes/index.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var router = express.Router();
+var getArticleList = require('../model/article.js').getArticleList
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+	getArticleList().then(articles => {
+		res.render('index', { title: 'Express', articles });
+	})
+  
+});
+
+module.exports = router;
